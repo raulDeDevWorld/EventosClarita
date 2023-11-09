d6ce83e1f52eda57b05d49a1bd010ef77868c132
raulDeDevWorld <proyection009@gmail.com>
2023-11-09 14:14:37 -0400

    DC

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,9 +9,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className='scroll-smooth'>
          <head>
-        <link rel="icon" href="/logo.png" />
+        <link rel="icon" href="/favicon.png" />
         <link rel='manifest' href='/manifest.json' />
-        <link rel='apple-touch-icon' href='/logo.png' />
+        <link rel='apple-touch-icon' href='/favicon.png' />
         <meta name="theme-color" content="#0090A8" />
         <meta name="msapplication-navbutton-color" content="#0090A8" />
         <meta name="apple-mobile-web-app-status-bar-style" content="#0090A8" />
